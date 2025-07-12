bbffcabd6d432376f0e0ae622846e1604e4e1132
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-07-12 10:15:08 +0300

    add "BGRABitmap"

--- .github/workflows/make.pas
+++ .github/workflows/make.pas
@@ -213,7 +213,7 @@ begin
   try
     if ParamCount > 0 then
       case ParamStr(1) of
-        'build': BuildAll(['BGRAControls']);
+        'build': BuildAll(['BGRABitmap', 'BGRAControls']);
         else
           OutLog(etError, ParamStr(1));
       end;
